6921b7ba5e976de4baf65be3857b55601b163a18
echonesis <echonesis@gmail.com>
2016-10-23 00:21:52 +0800

    test update v0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name = 'bcls-tk',
     packages = ['pkgA', 'pkgB'],
-    version = '0.0.1',
+    version = '0.0.2',
     description = 'Test for Binary Clustering Tool Kit',
     author = 'echonesis',
     author_email = 'echonesis@gmail.com',
